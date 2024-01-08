b77c806a29076cfdc6b1fb87edfe3b97af3c9dfa
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2024-01-08 15:34:42 +0900

    테스트

--- templates/NKStyle_Default/styles/default-manual-index.js
+++
@@ -1,5 +0,0 @@
-var currentPageUrl = window.location.href;
-var currentPageFileName = currentPageUrl.substring(currentPageUrl.lastIndexOf("/") + 1);
-if (currentPageFileName === "docs/index.html") {
-    window.location.href = "../manual/index.html";
-}
